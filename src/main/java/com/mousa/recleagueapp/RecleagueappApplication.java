7f07dbb062377635fbf938c051814ded1e771e83
MousaAdnan <mousaadnan1@gmail.com>
2025-11-16 06:55:34 -0500

    Add gitignore and secure application.properties

---
+++ src/main/java/com/mousa/recleagueapp/RecleagueappApplication.java
@@ -0,0 +1,13 @@
+package com.mousa.recleagueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecleagueappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecleagueappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mousa/recleagueapp/RecleagueappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mousa.recleagueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecleagueappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
