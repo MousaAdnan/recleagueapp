f0b91320510f5ee78d355e4da32ead2f0a6e8eb9
MousaAdnan <mousaadnan1@gmail.com>
2025-11-28 02:13:22 -0500

    blank changes

--- src/main/java/com/mousa/recleagueapp/repository/MatchPlayerStatsRepository.java
+++ src/main/java/com/mousa/recleagueapp/repository/MatchPlayerStatsRepository.java
@@ -3,6 +3,8 @@ package com.mousa.recleagueapp.repository;
 import com.mousa.recleagueapp.model.MatchPlayerStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchPlayerStatsRepository extends JpaRepository<MatchPlayerStats, Long> {
+import java.util.List;
 
+public interface MatchPlayerStatsRepository extends JpaRepository<MatchPlayerStats, Long> {
+    List<MatchPlayerStats> findbyMatchId(Long matchId);
 }
--- src/main/java/com/mousa/recleagueapp/service/FairTeamService.java
+++ src/main/java/com/mousa/recleagueapp/service/FairTeamService.java
@@ -1,4 +1,33 @@
 package com.mousa.recleagueapp.service;
 
+import com.mousa.recleagueapp.dto.FairTeamResult;
+import com.mousa.recleagueapp.model.Match;
+import com.mousa.recleagueapp.repository.MatchPlayerStatsRepository;
+import com.mousa.recleagueapp.model.MatchPlayerStats;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class FairTeamService {
+    private final MatchPlayerStatsRepository matchPlayerStatsRepository;
+
+    public FairTeamService(MatchPlayerStatsRepository matchPlayerStatsRepository) {
+        this.matchPlayerStatsRepository = matchPlayerStatsRepository;
+    }
+
+    public int ContributionsPerGame (){
+        return 0;
+    }
+
+    public FairTeamResult generateFairTeams(Long matchId) {
+
+        //List<MatchPlayerStats> statsForMatch = matchPlayerStatsRepository.findbyMatchId(matchId);
+
+        //algorithm goes here
+        //DFS with backtracking.
+
+        return new FairTeamResult(null, null, 0, 0);
+    }
+
+
 }
